7a11dc317f1e6abffd41189b8450d76742d8a9e5
Pedro <pedro.p.lara04@gmail.com>
2024-11-28 12:34:32 -0300

    Organizada nas rotas. Obs.: foi retirado navigation container para solucionar Render Error

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -15,31 +15,29 @@ import Registrations from '../pages/Registrations';
 import Activities from '../pages/Activities'
 import RegisterActivities from '../pages/Activities/RegisterActivities';
 import EditClasses from '../pages/Classes/EditClasses';
-import { NavigationContainer } from '@react-navigation/native';
-
+import EditSchool from '../pages/Schools/EditSchool';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes () {
     return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Schools" component={Schools} />
-                <Stack.Screen name="RecoverPassword" component={RecoverPassword} />
-                <Stack.Screen name="RegisterSchool" component={RegisterSchool} />
-                <Stack.Screen name="Students" component={Students} />
-                <Stack.Screen name="RegisterStudent" component={RegisterStudent} />
-                <Stack.Screen name="Classes" component={Classes} />
-                <Stack.Screen name="EditClasses" component={EditClasses} />
-                <Stack.Screen name="RegisterClasses" component={RegisterClasses} />
-                <Stack.Screen name="Registrations" component={Registrations} />
-                <Stack.Screen name="Activities" component={Activities} />
-                <Stack.Screen name="RegisterActivities" component={RegisterActivities} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="RecoverPassword" component={RecoverPassword} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Schools" component={Schools} />
+            <Stack.Screen name="RegisterSchool" component={RegisterSchool} />
+            <Stack.Screen name="EditSchool" component={EditSchool} /> 
+            <Stack.Screen name="Students" component={Students} />
+            <Stack.Screen name="RegisterStudent" component={RegisterStudent} />
+            <Stack.Screen name="Classes" component={Classes} />
+            <Stack.Screen name="RegisterClasses" component={EditClasses} />
+            <Stack.Screen name="EditClasses" component={EditClasses} />
+            <Stack.Screen name="Activities" component={Activities} />
+            <Stack.Screen name="RegisterActivities" component={RegisterActivities} />
+            <Stack.Screen name="Registrations" component={Registrations} />
+        </Stack.Navigator>
     );
 }
